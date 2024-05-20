6c2357becc5f6d898b8e569600ff4fc5f384a614
PrithivSP <prithivspn@gmail.com>
2024-05-20 22:22:49 +0530

    f:added popup window

--- web-template - Copy/src/components/WindowsDiv.js
+++ web-template - Copy/src/components/WindowsDiv.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import "./WindowsDiv.css"
+import { MdOutlineHistory } from "react-icons/md";
+import { LiaArrowRightSolid } from "react-icons/lia";
 
 function WindowsDiv({divVisibility}) {
   return (
-    <div className={`windows-wrapper-${divVisibility?"active":"inactive"}`}></div>
+    <div className={`windows-wrapper-${divVisibility?"active":"inactive"} windows-wrapper`}>
+      <div className='left'>
+        <p>Windows95</p>
+      </div>
+      <div className="right">
+        <ul className="program-lists">
+        <li>
+        <span><MdOutlineHistory /></span>
+            History
+        <span className='arrow'><LiaArrowRightSolid /></span>
+          </li>
+        <li>
+            Programs
+            <span className='arrow'><LiaArrowRightSolid /></span>
+          </li>
+        <li>
+            Documents
+            <span className='arrow'><LiaArrowRightSolid /></span>
+          </li>
+          <li>
+            Settings
+          </li>
+          <li>
+            Find
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
